0eca21e57a703bf90d5ffb5740f3f94c812cf4ce
elton <eltonm.prado@gmail.com>
2023-07-19 17:56:58 -0300

    Starting the Details page.

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,20 @@
+import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
+
+import { Container, Content } from './styles';
+
+export function Details(){
+  
+  return(
+    <Container>
+      <Header />
+
+      <Content>
+        <div>
+          <h1>FoodExplorer</h1>
+          <span>Por Elton Prado</span>
+        </div>
+      </Content>
+    </Container>
+  )
+}
---
+++ src/pages/Details/styles.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  
+  display: grid;
+  grid-template-rows: 10.4rem auto;
+  grid-template-areas: 
+  "header"
+  "content";
+`;
+
+export const Content = styled.div`
+  max-width: 136.8rem;
+  height: 100%;
+  padding-inline: 4rem;
+  margin-inline: auto;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  flex: 1;
+
+  @media (min-width: 768px) {
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+    justify-content: flex-start;
+    padding-inline: 12.3rem;
+    gap: 4rem;
+  }
+`;
