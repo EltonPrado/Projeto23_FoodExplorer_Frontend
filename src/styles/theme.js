3091deb8951e073c95ecce0991fad8c9691a9abb
elton <eltonm.prado@gmail.com>
2023-07-19 11:32:36 -0300

    creating application color token

---
+++ src/styles/theme.js
@@ -0,0 +1,25 @@
+export default {
+  COLORS: {
+    DARK_200: "#00070A", 
+    DARK_400: "#000A0F", 
+    DARK_600: "#00111A", 
+    DARK_700: "#001119",
+    DARK_800: "#0D161B",
+    DARK_900: "#0D1D25",
+    
+    LIGHT_100: "#FFFFFF",
+    LIGHT_200: "#FFFAF1",
+    LIGHT_300: "#E1E1E6",
+    LIGHT_400: "#C4C4CC", 
+    LIGHT_500: "#7C7C8A", 
+    LIGHT_700: "#4D585E",
+    
+    RED_100: "#750310", /* Cor - hover do botão */
+    RED_200: "#92000E", /* Cor - padrão dos botões */
+    RED_300: "#AB222E", /* Cor - hover do botão */
+    RED_400: "#AB4D55", /* Cor - salvar alterações */
+
+    BLUE_100: "#065E7C",
+    BLUE_200: "#82F3FF"
+  }
+};
