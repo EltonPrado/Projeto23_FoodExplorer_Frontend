d19af1bbcadd1ab9811acb062d7f0e96d47a3223
elton <eltonm.prado@gmail.com>
2023-07-19 12:10:55 -0300

    adding global styling in the application

---
+++ src/styles/global.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.COLORS.DARK_400};
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    font-size: 1.6rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  input {
+    outline: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter(0.2s);
+  }
+
+  button:hover, a:hover {
+    filter: brightness(0.9);
+  }
+
+  legend, button, a, h3, legend{
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+  }
+`;
