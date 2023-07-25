4123592cc579e13803f62c93479a9c3ec15faf3c
elton <eltonm.prado@gmail.com>
2023-07-25 12:06:35 -0300

    component creation: Input

---
+++ src/components/Input/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  gap: .8rem;
+
+  > input {
+    height: 4.8rem;
+    padding: 1.4rem;
+
+    font-size: 1.6rem;
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
+    background-color: ${({ theme }) => theme.COLORS.DARK_900};
+
+    border-radius: 0.8rem;
+    border: 0;
+
+    &::placeholder {
+      color: ${({ theme }) => theme.COLORS.LIGHT_500};
+      font-size: 1.6rem;
+    }
+  }
+`;
