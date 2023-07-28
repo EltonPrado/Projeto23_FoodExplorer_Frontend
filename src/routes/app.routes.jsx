0cbfd4271a93c04c8029714a22f9331f5a8dd73f
elton <eltonm.prado@gmail.com>
2023-07-28 15:26:26 -0300

    adding route profile

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route} from 'react-router-dom';
 
+import { Profile } from '../pages/Profile';
 import { Details } from '../pages/Details';
 
 export function AppRoutes() {
   return (
     <Routes>
+      <Route path="/profile" element={<Profile />}/>
       <Route path="/details/:id" element={<Details />}/>
     </Routes>
   )
