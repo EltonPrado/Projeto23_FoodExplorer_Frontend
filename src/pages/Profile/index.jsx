373aa222ab2161acffc0736dae0e2e24cec36390
elton <eltonm.prado@gmail.com>
2023-08-02 12:15:50 -0300

    adding user profile update.

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiChevronLeft } from 'react-icons/fi';
+
+import { useAuth } from '../../hooks/auth';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { Container, Form } from "./styles";
 
 export function Profile() {
+  const { user, updateProfile } = useAuth();
+
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [passwordOld, setPasswordOld] = useState();
+  const [passwordNew, setPasswordNew] = useState();
+
+  async function handleUpdate() {
+    const user = {
+      name, 
+      email, 
+      old_password: passwordOld,
+      password: passwordNew
+    }
+
+    await updateProfile({ user });
+  }
+
   return(
     <Container>
       <header>
@@ -16,24 +37,30 @@ export function Profile() {
         <Input 
           placeholder="Nome"
           type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input 
           placeholder="E-mail"
           type="text"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Senha atual"
           type="password"
+          onChange={e => setPasswordOld(e.target.value)}
         />
 
         <Input 
           placeholder="Nova senha"
           type="password"
+          onChange={e => setPasswordNew(e.target.value)}
         />
 
-        <Button title="Salvar" />
+        <Button title="Salvar" onClick={handleUpdate} />
       </Form>
     </Container>
   )  
