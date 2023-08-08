ddcdc4ffa7e9ed09c825a06b691d5b240396c386
elton <eltonm.prado@gmail.com>
2023-08-08 11:04:11 -0300

    component creation: Card.

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,103 @@
+import { api } from '../../services/api';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useAuth } from '../../hooks/auth';
+import { useFavorite } from '../../hooks/favorite';
+import { useCart } from '../../hooks/cart';
+
+import { PiPencilSimpleBold } from 'react-icons/pi'
+import { FiMinus, FiPlus } from 'react-icons/fi';
+import { FaAngleRight } from 'react-icons/fa'
+
+import { Container } from './styles';
+import { Button } from '../Button';
+
+import heart from '../../assets/heart.svg';
+import heartFill from '../../assets/heart_fill.svg';
+
+
+export function Card({data, ...rest}) {
+  const [quantity, setQuantity] = useState(1);
+
+  const { user } = useAuth();
+  const { favorites, addFoodToFavorite, removeFoodFromFavorite } = useFavorite()
+  const { handleAddFoodToCart } = useCart();
+  
+  const isFavorite = favorites.some((food) => food.title === data.title)
+
+  const navigate = useNavigate()
+
+  const imageURL = `${api.defaults.baseURL}/files/${data.image}`;
+
+  function handleAddQuantity() {
+    const isGreater10 = quantity >= 9;
+    if(isGreater10) {
+      return
+    }
+
+    setQuantity(quantity + 1);
+  }
+
+  function handleRemoveQuantity() {
+    const isLess0 = quantity <= 1;
+    if (isLess0) {
+      return
+    }
+    
+    setQuantity(quantity - 1);
+  }
+
+  function handleDetails(id) {
+    navigate(`/details/${id}`);
+  }
+
+  function handleEditFood(id) {
+    navigate(`/edit/${id}`);
+  }
+
+  return (
+    <Container {...rest}>
+      {
+        user.isAdmin ? 
+        <button onClick={handleEditFood}>
+          <PiPencilSimpleBold size={25}/>
+        </button>
+        :
+        <button type='button' onClick={() => isFavorite ? removeFoodFromFavorite(data) : addFoodToFavorite(data)}> 
+          <img src={isFavorite ?  heartFill : heart} alt="heart" />  
+        </button>
+      }
+      
+      <div>
+        <img src={imageURL} alt={data.title} />
+      </div>
+
+      <a type='button' onClick={handleDetails}>
+        <h3>{data.title} <FaAngleRight /></h3>
+      </a>
+
+      <p>{data.description}</p>
+      <strong>R$ {data.price}</strong>
+
+      {
+        user.isAdmin ?
+        <div></div>
+        :
+        <div>
+          <button className="btn" onClick={handleRemoveQuantity}>
+            <FiMinus size={25}/>
+          </button>
+          
+          <span>0{quantity}</span>
+          
+          <button className="btn" onClick={handleAddQuantity}>
+            <FiPlus size={25}/>
+          </button>
+
+          <Button title="incluir" onClick={() => handleAddFoodToCart(data, quantity, imageURL)} />
+        </div>
+      } 
+    </Container>
+  )
+}
---
+++ src/components/Card/styles.js
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 30rem;
+  height: 46.2rem;
+  border-radius: 0.8rem;
+  background-color: ${({ theme }) => theme.COLORS.DARK_200};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  flex: none;
+
+  > button:first-child {
+    display: flex;
+    position: absolute;
+    top: 1.6rem;
+    right: 1.8rem;
+
+    background: none;
+    border: none;
+
+    svg {
+      fill: ${({ theme, isFavorite }) => isFavorite ? theme.COLORS.RED_200 : theme.COLORS.LIGHT_100};
+    }
+  }
+
+  > div:nth-child(2) {
+    width: 17.6rem;
+    height: 17.6rem;
+    img {
+      width: 100%;
+    }
+  }
+
+  > a {
+    h3 {
+    margin: 1.5rem 0;
+    font-size: 2.4rem;
+    font-weight: bold;
+
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+
+    display: flex;
+    align-items: center;
+    }
+  }
+
+  > p {
+    text-align: center;
+    font-size: 1.2rem;
+    padding-inline: 4rem;
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
+  }
+
+  > strong {
+    font-size: 3.2rem;
+    color: ${({ theme }) => theme.COLORS.BLUE_200};
+    margin-block: 1.5rem;
+  }
+
+  > div:last-child {
+    display: flex;
+    align-items: center;
+    gap: 1.6rem;
+    width: 20.8rem;
+
+    position: relative;
+    z-index: 10;
+
+    span {
+      font-size: 2rem;
+      font-weight: bold;
+    }
+    
+    .btn {
+      background: none;
+      border: none;
+      color: ${({ theme }) => theme.COLORS.LIGHT_100};
+
+      display: flex;
+      align-items: center;
+
+      position: relative;
+
+      z-index: 6;
+    }
+  }
+`
