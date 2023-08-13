51ce7376ac4e75a96dc6777426c887e2ce427a53
elton <eltonm.prado@gmail.com>
2023-08-12 22:05:51 -0300

    hook creation: favorite.

---
+++ src/hooks/favorite.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+
+export const FavoriteContext = createContext({});
+
+function FavoriteProvider({ children }) {
+  const [favorites, setFavorites] = useState(JSON.parse(localStorage.getItem(`@foodexplorer: favorites`)) || []);
+
+  function addFoodToFavorite(data) {
+    setFavorites([...favorites, data])
+  }
+
+  function removeFoodFromFavorite(data) {
+    setFavorites(favorites.filter((food) => food.id !== data.id))
+  }
+
+  useEffect(() => {
+    localStorage.setItem(`@foodexplorer: favorites`, JSON.stringify(favorites));
+  }, [favorites])
+
+  return (
+    <FavoriteContext.Provider value={{ 
+      favorites,
+      addFoodToFavorite,
+      removeFoodFromFavorite
+    }}>
+      { children }
+    </FavoriteContext.Provider>
+  )
+}
+
+function useFavorite() {
+  const context = useContext(FavoriteContext);
+  return context;
+}
+
+export { FavoriteProvider, useFavorite };
