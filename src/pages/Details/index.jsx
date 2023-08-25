18820291f9e4ffb41d701cf8e70505036c340019
elton <eltonm.prado@gmail.com>
2023-08-25 10:16:31 -0300

    file refactoring.

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,20 +1,124 @@
+import { api } from '../../services/api';
+import { useCart } from '../../hooks/cart';
+import { useAuth } from '../../hooks/auth';
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
 import { Header } from '../../components/Header';
+import { Ingredient } from '../../components/Ingredient';
+import { Button } from '../../components/Button';
 import { Footer } from '../../components/Footer';
 
-import { Container, Content } from './styles';
+import { FiMinus, FiPlus, FiChevronLeft } from 'react-icons/fi';
+import { Container, Content, ButtonBack, Main, Ingredients, Info } from './styles';
 
 export function Details(){
+  const [data, setData] = useState(null);
+  const [quantity, setQuantity] = useState(1);
+
+  const { user } = useAuth();
+  const params = useParams();
+  const navigate = useNavigate();
   
+  const { handleAddFoodToCart } = useCart();
+
+  const imageURL = data && `${api.defaults.baseURL}/files/${data.image}`;
+
+  function handleBack() {
+    navigate(-1);
+  }
+
+  function handleEditFood(id) {
+    navigate(`/edit/${id}`);
+  }
+  
+  function handleAddQuantity() {
+    const isGreater10 = quantity >= 9;
+    if(isGreater10) {
+      return
+    }
+
+    setQuantity(quantity + 1);
+  }
+
+  function handleRemoveQuantity() {
+    const isLess0 = quantity <= 1;
+    if (isLess0) {
+      return
+    }
+    setQuantity(quantity - 1);
+  }
+
+  useEffect(() => {
+    async function fetchFood() {
+      const response = await api.get(`/foods/${params.id}`);
+      setData(response.data);
+    }
+    
+    fetchFood();
+  }, []);
+
   return(
     <Container>
       <Header />
 
       <Content>
-        <div>
-          <h1>FoodExplorer</h1>
-          <span>Por Elton Prado</span>
-        </div>
+        <ButtonBack onClick={handleBack}>
+          <span><FiChevronLeft size={32}/>Voltar</span>
+        </ButtonBack>
+
+        {data &&
+          <Main>
+            <div className="FoodPreview">
+              <img src={imageURL} alt="imagem do prato" />
+            </div>
+
+            <div className="foodInfo">
+              <h1>{data.title}</h1>
+              <p>{data.description}</p>
+
+              <Ingredients>
+                {data.ingredients.map((ingredient) => (
+                    <Ingredient 
+                      key={String(ingredient.id)} 
+                      ingredient={ingredient.name} 
+                    />
+                  ))}
+              </Ingredients>
+
+              {user.isAdmin === 0 && (
+                <Info>
+                  <div className="foodEdit">
+                    <Button title="Editar Prato" onClick={() => handleEditFood(data.id)} />
+                  </div>
+                </Info>
+              )}
+
+              {user.isAdmin === 1 && (
+                <Info>
+                  <button className="btn" onClick={handleRemoveQuantity}> 
+                      <FiMinus size={25} />
+                    </button>
+                
+                    <span>0{quantity}</span>
+                
+                    <button className="btn" onClick={handleAddQuantity}>
+                      <FiPlus size={25} />
+                    </button>
+                    
+                    <div>
+                      <Button 
+                        title={`incluir - R$ ${data.price}`}
+                        onClick={() => handleAddFoodToCart(data, quantity, imageURL)}
+                      />
+                    </div>
+                </Info>
+              )}
+            </div>
+          </Main>
+        }
       </Content>
+
       <Footer />
     </Container>
   )
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -5,7 +5,7 @@ export const Container = styled.div`
   min-height: 100vh;
   
   display: grid;
-  grid-template-rows: 10.4rem auto;
+  grid-template-rows: 10.4rem auto 12.4rem;
   grid-template-areas: 
   "header"
   "content"
@@ -32,5 +32,130 @@ export const Content = styled.div`
     justify-content: flex-start;
     padding-inline: 12.3rem;
     gap: 4rem;
+  }
+`;
+
+export const ButtonBack = styled.div`
+  width: 100%;
+  margin: 2.4rem auto 4rem;
+  display: flex;
+  
+  > span {
+    display: flex;
+    align-items: center;
+    font-size: clamp(1.8rem, .4rem + 3vw, 2.4rem);
+
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    cursor: pointer;
+  }
+
+  @media (min-width: 768px) {
+    padding-inline: 0;
+  }
+`;
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .FoodPreview {
+    max-width: 25rem;
+    border-radius: 50%;
+
+    img {
+      max-width: 100%;
+    }
+  }
+
+  > .foodInfo {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      font-family: 'Poppins', sans-serif;
+
+    h1 {
+      margin-top: 2rem;
+      margin-bottom: 1.8rem;
+      font-size: clamp(2.4rem, 1rem + 3vw, 4rem);
+      font-weight: 700;
+    }
+
+    p {
+      text-align: center;
+      font-size: clamp(1.8rem, .4rem + 3vw, 2.4rem);
+      color: ${({ theme }) => theme.COLORS.LIGHT_300};
+    }
+  }
+
+  @media (min-width: 768px) {
+    width: 100%;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 4rem;
+
+    > .FoodPreview {
+      max-width: 39rem;
+      border-radius: 50%;
+    }
+
+    > .foodInfo {
+      max-width: 60rem;
+      align-items: flex-start;
+
+      h1 {
+        margin-top: 0;
+      }
+
+      p {
+        text-align: left;
+      }
+    }
+  }
+`;
+
+export const Ingredients = styled.div`
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  margin-block: 3rem;
+`;
+
+export const Info = styled.div`
+  width: 100%;
+  
+  display: flex;
+  align-items: center;
+  margin-top: 2rem;
+  padding-inline: 2rem;
+  gap: 1.7rem;
+
+  span {
+    font-size: 2rem;
+    font-weight: 700;
+  }
+    
+  .btn {
+    display: flex;
+    align-items: center;
+
+    background: none;
+    border: none;
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+
+    position: relative;
+    z-index: 6;
+  }
+
+  @media (min-width: 768px) {
+    padding-inline: 0;
+     
+    > strong{
+      min-width: 14rem;
+    } 
   }
 `;
