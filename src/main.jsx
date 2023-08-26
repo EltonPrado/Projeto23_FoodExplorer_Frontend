d3b1bb5f1eec4b2c70d8e03544c514e1b372cf60
elton <eltonm.prado@gmail.com>
2023-08-26 11:31:23 -0300

    main file refactoring.

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,19 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 
+import { AuthProvider } from './hooks/auth';
+import { FavoriteProvider } from './hooks/favorite';
 import { Routes } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <AuthProvider>
+        <FavoriteProvider>
+          <Routes />
+        </FavoriteProvider>
+      </AuthProvider>
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
