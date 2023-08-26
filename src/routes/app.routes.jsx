a2c7db353c6601ece726bf07b9b69605dea205d8
elton <eltonm.prado@gmail.com>
2023-08-26 11:29:12 -0300

    application route refactoring.

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,13 +1,19 @@
 import { Routes, Route} from 'react-router-dom';
 
+import { Home } from '../pages/Home';
 import { Profile } from '../pages/Profile';
 import { Details } from '../pages/Details';
+import { New } from '../pages/New';
+import { Edit } from '../pages/Edit';
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/profile" element={<Profile />}/>
-      <Route path="/details/:id" element={<Details />}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/details/:id" element={<Details />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/edit/:id" element={<Edit />} />
     </Routes>
   )
 }
