d6ff26e36b4b259bde7a0dde43e6e95c9c1b9102
elton <eltonm.prado@gmail.com>
2023-08-28 19:28:01 -0300

    Ingredient component refactoring.

--- src/components/Ingredient/styles.js
+++ src/components/Ingredient/styles.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 export const Container = styled.span`
   font-size: 1.4rem;
-  padding: .5rem .8rem;
+  font-weight: 500;
+  padding: .4rem .8rem;
   border-radius: .5rem;
-  margin-right: .6rem;
 
   color: ${({ theme }) => theme.COLORS.LIGHT_100};
   background-color: ${({ theme }) => theme.COLORS.DARK_1000};
