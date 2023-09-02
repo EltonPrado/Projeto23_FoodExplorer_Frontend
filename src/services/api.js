2caa4547286c2aa76f4f1a91a0f89975f02633c3
elton <eltonm.prado@gmail.com>
2023-09-02 08:45:01 -0300

    inserting production environment route.

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  //baseURL: 'http://localhost:3333' - ambiente Local
+  baseURL: 'https://foodexplorer-api-qbp5.onrender.com'
 });
